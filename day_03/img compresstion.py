378f35775b63a2f028c64664101d9c261f3db291
kanimozhikk09 <kanimozhikk09@gmail.com>
2025-08-20 15:41:43 +0530

    Add files via upload

---
+++ day_03/img compresstion.py
@@ -0,0 +1,4 @@
+import cv2
+img=cv2.imread("1.jpg")
+cv2.imwrite("compressed_image_30.jpeg",img,[int(cv2.IMWRITE_JPEG_QUALITY),30])
+cv2.imwrite("compressed_image_90.jpeg",img,[int(cv2.IMWRITE_JPEG_QUALITY),90])
